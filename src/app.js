c2df365279adeefe72aedae49af348a33d7097e1
Mohammed Ajsal <ajsal12aju@gmail.com>
2024-10-02 12:20:57 +0530

    updated

---
+++ src/app.js
@@ -0,0 +1,14 @@
+ const express = require('express')
+
+ const app  = express()
+ // creatrinng new web server
+
+
+ app.use((req,res)=>{
+    res.send("heloo from the server")
+ })
+
+// listening the server for waiting incoming services
+ app.listen(3000, ()=>{
+    console.log("server is started")
+ })
