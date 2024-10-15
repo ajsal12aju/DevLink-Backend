53903bf722803f235fac1e233bf363def404bfc7
Mohammed Ajsal <ajsal12aju@gmail.com>
2024-10-15 22:09:13 +0530

    updated

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,24 +1,44 @@
-const express =  require("express");
+const express = require("express");
 const { userAuth } = require("../middlewares/auth");
 const CannotonnectionRequest = require("../models/connectionRequest");
 
-
-
 const userRouter = express.Router();
 
-userRouter.get("/user/requests/recieved", userAuth, async (req,res)=>{
-    try {
-        const loggedUser = req.user;
+userRouter.get("/user/requests/recieved", userAuth, async (req, res) => {
+  try {
+    const loggedUser = req.user;
 
-      const datas = await CannotonnectionRequest.find({
-        toUserId: loggedUser._id,
-        status: "interested",
-      });
+    const datas = await CannotonnectionRequest.find({
+      toUserId: loggedUser._id,
+      status: "interested",
+    }).populate("fromUserId", ["firstName", "lastName"]);
 
-      res.status(200).json({data:datas, message:"Data Fetched successfully"});
-    } catch (error) {
-        res.status(400).send("ERROR:" + error.message)
-    }
-})
+    res.status(200).json({ data: datas, message: "Data Fetched successfully" });
+  } catch (error) {
+    res.status(400).send("ERROR:" + error.message);
+  }
+});
 
-module.exports = userRouter
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+  try {
+    const loggedUser = req.user;
+
+    const allaccepetedConnections = await CannotonnectionRequest.find({
+      $or: [
+        { fromUserId: loggedUser._id, status: "accepted" },
+        { toUserId: loggedUser._id, status: "accepted" },
+      ],
+    })
+      .populate("fromUserId", ["firstName", "lastName"])
+      .populate("toUserId", ["firstName", "lastName"]);
+    const data = allaccepetedConnections.map((row)=> {
+        if(row.fromUserId._id.toString() === loggedUser._id.toString()){
+            return row.toUserId;
+        }
+        return row.fromUserId
+    });
+  
+    res.json({ data: data });
+  } catch (error) {}
+});
+module.exports = userRouter;
